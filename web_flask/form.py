1c08ec863c363d4ad9b7a07ff36a9f571b31146f
faith2221 <edithbanda014@gmail.com>
2024-03-22 03:14:36 -0700

    Additions

--- web_flask/form.py
+++ web_flask/form.py
@@ -3,10 +3,12 @@ from wtfforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DtaRequired, Email, Length
 
 
+# Registration Form Class
 class RegistrationForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=8, max=15)])
     email = StringField('First Name', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
@@ -14,12 +16,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 
+# Login Form Class
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
 
+# UserProfile Form Class
 class UserProfileForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
@@ -29,6 +33,7 @@ class UserProfileForm(FlaskForm):
     submit = SubmitField('Update Profile')
 
 
+#Post Form Class
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
@@ -37,6 +42,7 @@ class PostForm(FlaskForm):
     delete = SubmitField('Delete')
 
 
+# Comment Form Class
 class CommentForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Update Comment')
