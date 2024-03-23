79d18932e02332c0aeabc1e62153018901298459
faith2221 <edithbanda014@gmail.com>
2024-03-23 01:17:08 -0700

    Addition of validation code

--- web_flask/form.py
+++ web_flask/form.py
@@ -18,11 +18,38 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 
+    def validate_username(self, username):
+    """ Validates the username whether it's already taken or not."""
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+        raise ValidationError('This username is already taken.
+                              Please choose a different one.')
+
+    def validate_password(self, password):
+    """ Validates the username whether it's already taken or not."""
+    user = User.query.filter_by(password=password.data).first()
+    if user:
+        raise ValidationError('This password is already taken.
+                              Please choose a different one.')
+
+    def validate_password_strength(self, password):
+    """ Validates the strength of the password."""
+    patthern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)
+                (?=.*[@$!%*?&])[A-Za-z\d@$!%*?*]{8,}$'
+    if not re.match(pattern, password.data):
+        raise ValidationError('Password must be atleast 8 characters long,
+                              and include atleast one uppercase letter,
+                              one lowercase letter,
+                              one digit, and one special character.')
+
+
+
 # Login Form Class
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
+
 
 
 # UserProfile Form Class
