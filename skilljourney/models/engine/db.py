eb915915bdbeab1bc8fdd28ef2bbfe5a937dc492
faith2221 <edithbanda014@gmail.com>
2024-03-25 03:49:24 -0700

    Delets

--- skilljourney/models/engine/db.py
+++ skilljourney/models/engine/db.py
@@ -1,18 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship, scoped_session,sessionmaker
-
 
 # Create SQLAlchemy database instance
 db = SQLAlchemy()
-
-
-# Define the engine for SQLAlchemy
-engine = create_engine("sqlite:///app.db", echo=True)
-
-
-# Create a scoped session for Flassk-SQLAlchemy
-Session = scoped_session(sessionmaker(bind=engine))
 
 
 # Import model classes after creating the db instance
